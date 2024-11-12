90b18519f84889fa0a3c7145808510761d4f4aa1
bayuadjy <bayajinugroho@gmail.com>
2024-11-12 19:41:04 +0700

    Create Penerima.java

---
+++ Penerima.java
@@ -0,0 +1,19 @@
+package ZakatMal;
+
+public class Penerima {
+        private String nama;
+        
+        public Penerima(){
+            nama = "Tidak diketahui";
+        }
+        
+        public String setNama(String nama){
+            return this.nama = nama;
+        }
+        
+        public String getNama(){
+            return nama;
+        }
+        
+        
+}
